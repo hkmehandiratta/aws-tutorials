16ee7ca85394890cbbe2486bdf86a9b003c547de
hkmehandiratta <hkmehandiratta@yahoo.com>
2022-04-21 17:14:29 +0530

    Update server_start.sh

--- awspipelinedemo/scripts/server_start.sh
+++ awspipelinedemo/scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo chmod +x /opt/tomcat/apache-tomcat-8.5.78/bin/./startup.sh
-sudo /opt/tomcat/apache-tomcat-8.5.78/bin/./startup.sh
+sudo chmod +x /opt/tomcat/bin/./startup.sh
+sudo /opt/tomcat/bin/./startup.sh
